7cc0a2b335d798cc5f9acf55843e026bf6ff2f91
eliansanchez00 <elianjss68@gmail.com>
2026-04-24 21:06:57 -0300

    ajsd

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ const ticketDonacionRespondido = new Map();
 const ticketAutoRespondido = new Map();
 const giveaways = new Map();
 
-const IMG = "https://i.postimg.cc/Hx50gSWf/F9F37C29-1F1A-4353-B1A7-DF8FA7CB2445.png";
+const IMG = "https://freeimage.host/i/B60hMmu";
 
 // Verifica si el miembro tiene alguno de los roles de staff
 const hasStaff = (member) =>
